4a6d8b67b5aed34fb9cad1a932d3d2e037d91f8a
Alexander Orzechowski <alex@ozal.ski>
2023-06-02 14:35:26 -0400

    scene_graph: Port ext_session_v1

--- sway/input/seat.c
+++ sway/input/seat.c
@@ -1044,17 +1044,8 @@ void seat_configure_xcursor(struct sway_seat *seat) {
 
 bool seat_is_input_allowed(struct sway_seat *seat,
 		struct wlr_surface *surface) {
-	if (server.session_lock.locked) {
-		if (server.session_lock.lock == NULL) {
-			return false;
-		}
-		struct wlr_session_lock_surface_v1 *lock_surf;
-		wl_list_for_each(lock_surf, &server.session_lock.lock->surfaces, link) {
-			if (lock_surf->surface == surface) {
-				return true;
-			}
-		}
-		return false;
+	if (server.session_lock.lock) {
+		return sway_session_lock_has_surface(server.session_lock.lock, surface);
 	}
 	struct wl_client *client = wl_resource_get_client(surface->resource);
 	return seat->exclusive_client == client || seat->exclusive_client == NULL;
@@ -1262,8 +1253,8 @@ void seat_set_focus(struct sway_seat *seat, struct sway_node *node) {
 	} else {
 		seat_set_workspace_focus(seat, node);
 	}
-	if (server.session_lock.locked) {
-		seat_set_focus_surface(seat, server.session_lock.focused, false);
+	if (server.session_lock.lock) {
+		seat_set_focus_surface(seat, server.session_lock.lock->focused, false);
 	}
 }
 
